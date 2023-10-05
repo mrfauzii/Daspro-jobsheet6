9aac17dd8ebe044180d6aa802e50c0436b6ec359
mrfauzii <reishifauzi24@gmail.com>
2023-10-05 15:11:16 +0700

    modifikasi

--- Pemilihan2Percobaan119.java
+++ Pemilihan2Percobaan119.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
-public class Pemilihan2Percobaan119{
+public class Pemilihan2Percobaan119 {
 
-    public static void main (String []args){
-        Scanner input19 =new Scanner(System.in);
-
-        System.out.println("Masukan Tahun   :");
-        int tahun =input19.nextInt();
-
+    public static void main(String[] args) {
+        Scanner input19 = new Scanner(System.in);
+        System.out.println("masukan tahun");
+        int tahun = input19.nextInt();
+        
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) !=0 )
-            System.out.println("Tahun Kabisat");
-        } else
-        System.out.println("Bukan Tahun Kabisat");
-
-
+            if (((tahun % 100) != 0) || ((tahun % 400) == 0)) {
+                System.out.println("Tahun Kabisat");
+            } else {
+                System.out.println("Bukan Tahun Kabisat");
+            }
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+        }        
     }
-}
+}        
