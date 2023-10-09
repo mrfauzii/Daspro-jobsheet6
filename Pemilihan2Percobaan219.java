ae668d67d409fed713d126245df42c9c6e8c94f5
mrfauzii <reishifauzi24@gmail.com>
2023-10-09 15:27:55 +0700

    modifikasi percobaan 2

--- Pemilihan2Percobaan219.java
+++ Pemilihan2Percobaan219.java
@@ -14,17 +14,28 @@ public class Pemilihan2Percobaan219 {
 
         totalSudut = sudut1 + sudut2 + sudut3;
 
-        if (totalSudut == 180) {
+        if (totalSudut == 180)
+        {
             if ((sudut1 == 90) ||(sudut2 == 90)|| (sudut3 ==90))
             System.out.println("Segitiga tersebut adalah segitiga siku-siku");
+           
+            
+            else if ((sudut1 == 60) && (sudut2 == 60) && (sudut3 == 60))
+            {
+                System.out.println("Segitiga tersebut adalah segitiga sama sisi");
+            }
+            else if ((sudut1 == sudut2) || (sudut2 == sudut3) || (sudut3 == sudut1))
+            {
+                System.out.println("Segitiga tersebut adalah segitiga sama kaki");
+            }
             else
-                System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");       
-        } else
-            System.out.println ("Bukan Setiga");
-        
-
-
-
-
-    }
+            {
+                System.out.println("Segitiga tersebut adalah segitiga sembarang");
+            }
+        }
+        else
+        {
+            System.out.println("Bukan Segitiga");
+        }
+    }    
 }
